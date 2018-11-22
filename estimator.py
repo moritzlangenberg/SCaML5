c2b6c2fb48766ccc10d41a5ce06e3797f55bae5a
Moritz <moritzlangenberg@ymail.com>
2018-11-22 21:29:29 +0100

    task 4

--- estimator.py
+++ estimator.py
@@ -14,7 +14,7 @@ K = int(data[0]) #number of classes
 D = int(data[1]) #number of dimensions
 N = [0] * K #number of observations of every class k
 #k = 0 #class of following obervation vector
-sigma = [0] * D # we "simulate" diagonal matrix
+sigmaPD = [0] * D # we "simulate" diagonal matrix
 means = [[0]*K]*D
 
 #functions
@@ -52,7 +52,7 @@ def mean(k):
 #    return [pooledVariance(d) for d in range(0,D)]
 
 
-def pooledCovarianceVector():
+def pooledDiagonalCovariance():
     varianceX = [0] * D
     for observationIterator in range(0, numberOfAllObservations):
         currentClass = int(data[observationIterator * 257 + 2])
@@ -67,6 +67,90 @@ def pooledCovarianceVector():
     sigma = [np.divide(x, numberOfAllObservations) for x in varianceX]
     return sigma
 
+
+################EXERCISE 4#######################
+def classSpecificDiagonalCovariance():
+    varianceX = [[0] * D] * K
+    classCount = [0] * K
+    for observationIterator in range(0, numberOfAllObservations):
+        currentClass = int(data[observationIterator * 257 + 2])
+        if currentClass == 10:
+            currentClass = 0
+        classCount[currentClass] += 1
+        currentObservation = observationIterator * 257 + 3
+        for elementIterator in range(0, D):
+            tempX = int(data[currentObservation + elementIterator])
+            tempMean = means[currentClass][elementIterator]
+            varianceX[elementIterator][currentClass] += (tempX - tempMean)**2
+
+    for k in range(0, K):
+        for x in range(0, K):
+            varianceX[x][k] = np.divide(varianceX[x][k], classCount[k])
+    return varianceX
+
+def pooledFullCovariance():
+    covarianceMatrix = [[0] * D] * D
+    for observationIterator in range(0, numberOfAllObservations):
+        currentClass = int(data[observationIterator * 257 + 2])
+        if currentClass == 10:
+            currentClass = 0
+        currentObservation = observationIterator * 257 + 3
+        for elementIterator in range(0, D):
+            tempX = int(data[currentObservation + elementIterator])
+            tempMean = means[currentClass][elementIterator]
+            for secondElementIterator in range(0, D):
+                tempX2 = int(data[currentObservation + secondElementIterator])
+                tempMean2 = means[currentClass][secondElementIterator]
+                covarianceMatrix[elementIterator][secondElementIterator] += (tempX - tempMean)*(tempX2 - tempMean2)
+
+    for x in range(0,D):
+        for y in range(0,D):
+            covarianceMatrix[x][y] = np.divide(covarianceMatrix[x][y], numberOfAllObservations)
+
+    return covarianceMatrix
+
+def classSpecificFullCovariance():
+    covarianceMatrix = [[0] * D] * D
+    for observationIterator in range(0, numberOfAllObservations):
+        currentClass = int(data[observationIterator * 257 + 2])
+        if currentClass == 10:
+            currentClass = 0
+        currentObservation = observationIterator * 257 + 3
+        for elementIterator in range(0, D):
+            tempX = int(data[currentObservation + elementIterator])
+            tempMean = means[currentClass][elementIterator]
+            for secondElementIterator in range(0, D):
+                tempX2 = int(data[currentObservation + secondElementIterator])
+                tempMean2 = means[currentClass][secondElementIterator]
+                covarianceMatrix[elementIterator][secondElementIterator] += (tempX - tempMean)*(tempX2 - tempMean2)
+
+    for x in range(0,D):
+        for y in range(0,D):
+            covarianceMatrix[x][y] = np.divide(covarianceMatrix[x][y], numberOfAllObservations)
+
+    return covarianceMatrix
+
+
+echo "# SCaML" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/moritzlangenberg/SCaML.git
+git push -u origin master
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################################################
 def p(k):
     return np.divide(N[k], numberOfAllObservations)
 
@@ -99,7 +183,7 @@ for k in range(0,K):
         out.write(" ")
     out.write("\n")
 
-    for v in pooledCovarianceVector():
+    for v in pooledDiagonalCovariance():
         out.write(str(v))
         out.write(" ")
     out.write("\n")
